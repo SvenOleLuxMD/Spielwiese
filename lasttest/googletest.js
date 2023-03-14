6d11cdd86535993d57cd840fc3966f69770a35b6
Sven Ole Lux <74190745+SvenOleLuxMD@users.noreply.github.com>
2023-03-14 13:09:53 +0100

    Add files via upload

---
+++ lasttest/googletest.js
@@ -0,0 +1,28 @@
+
+import { check } from 'k6';
+import http from "k6/http";
+export let options = {
+    vus: 5,
+    stages: [
+        { duration: "10s", target: 10 },
+        { duration: "10s", target: 10 },
+        { duration: "10s", target: 50 },
+        { duration: "10s", target: 50 },
+        { duration: "10s", target: 0 },
+    ],
+    thresholds: {
+      // the rate of successful checks should be higher than 90%
+      checks: ['rate>0.99'],
+  
+    },
+};
+
+export default function() {
+    let response = http.get("https://google.com");
+    check(response, {
+
+        'is status 200': (r) => r.status === 200,
+    
+      });
+};
+
