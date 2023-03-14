58b02d30e533eefd3a49e09fd61b68aa5c175a43
Sven Ole Lux <74190745+SvenOleLuxMD@users.noreply.github.com>
2023-03-14 12:59:29 +0100

    Update mandel.js

--- lasttest/mandel.js
+++ lasttest/mandel.js
@@ -1,15 +1,15 @@
 
 import { check } from 'k6';
 import http from "k6/http";
-// export let options = {
-//     vus: 5,
-//     stages: [
-//         { duration: "10s", target: 10 },
-//         { duration: "10s", target: 10 },
-//         { duration: "10s", target: 3000 },
-//         { duration: "10s", target: 0 },
-//     ]
-// };
+ export let options = {
+     vus: 5,
+     stages: [
+         { duration: "10s", target: 10 },
+         { duration: "10s", target: 10 },
+         { duration: "10s", target: 50 },
+         { duration: "10s", target: 0 },
+     ]
+ };
 
 export default function() {
     let response = http.get("https://mandel-dev.md-abr-np.k8s.freenet-group.de/v1/common/network/networkDefinitions");
